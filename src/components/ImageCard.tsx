2667dcfff8fb99ba10806790050eecdd9b9bc108
Sonu Ram <sonuram6960@gmail.com>
2020-05-10 11:53:41 +0530

    Complete pics project

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -7,9 +7,13 @@ interface Props {
 
 class ImageCard extends React.Component<Props> {
   imageRef: any;
+  state: {
+    spans: number;
+  };
 
   constructor(props: Props) {
     super(props);
+    this.state = { spans: 0 };
     this.imageRef = React.createRef();
   }
 
@@ -18,14 +22,18 @@ class ImageCard extends React.Component<Props> {
   }
 
   setGridSpans = () => {
-    console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 20);
+
+    this.setState({ spans });
   };
 
   render() {
     const { urls, description } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
