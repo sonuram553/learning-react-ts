63319234a007a323ec2b73017a113582af25cfa9
Sonu Ram <sonuram6960@gmail.com>
2020-07-10 20:42:10 +0530

    Add UI to select language in APP component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 
-class App extends React.Component {
+interface State {
+  language: string;
+}
+
+interface Props {}
+
+class App extends React.Component<Props, State> {
+  state: State = { language: "english" };
+
   render() {
-    return <div>App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.setState({ language: "english" })}
+          ></i>
+          <i
+            className="flag nl"
+            onClick={() => this.setState({ language: "dutch" })}
+          ></i>
+        </div>
+      </div>
+    );
   }
 }
 
